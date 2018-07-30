0b1d7ae7c77577d65df503c062a6956c3f4a405d
brigadin <brigadin@mail.com>
2018-07-30 10:03:24 +0300

    initial server

--- src/main/java/com/qobiko/server/controler/ReminderControler.java
+++ src/main/java/com/qobiko/server/controler/ReminderControler.java
@@ -3,16 +3,22 @@ package com.qobiko.server.controler;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/remind")
 public class ReminderControler {
 
     @RequestMapping(value ="/get",method = RequestMethod.GET)
-    public String getRemainder()
+    @ResponseBody
+    public String getRemainder(ModelMap model)
     {
         return "my remainder";
+
+
+       //add line
     }
 }
